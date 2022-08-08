3304836d0ff96c725b5093a35ed9cec758e1c7a4
Tristan Schlarman <44757288+periurium@users.noreply.github.com>
2022-08-08 15:36:32 -0400

    Bloatware & App Associations

---
+++ NSSS.ps1
@@ -0,0 +1,11 @@
+﻿# A Remake of Nates "System Setup Script"
+
+# Check if Script is run as Administrator. If not, elevate
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.Windows.BuiltInRole]'Administrator')) {
+
+    Write-Host "Elevating to Administrator Privileges In 5 seconds"
+    Start-Sleep 5
+    Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit
+
+}
